b5d189004cccf7a3031fe3480d5aa6743e986732
luantreia <luantreia@gmail.com>
2025-05-17 03:04:56 -0300

    Auto deploy

--- models/Equipo.js
+++ models/Equipo.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const equipoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
+  escudo: { type: String },
+  foto: { type: String},
 });
 
 export default mongoose.model('Equipo', equipoSchema);
