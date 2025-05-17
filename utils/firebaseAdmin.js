d76d5cb88216f8b5682a964c6a54bd143dc68390
luantreia <luantreia@gmail.com>
2025-05-17 01:20:20 -0300

    cambio en import de firebaseAuth

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,11 +1,11 @@
-// ./utils/firebaseAdmin.js
 import admin from 'firebase-admin';
 
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-
+if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+}
 
 export default admin;
