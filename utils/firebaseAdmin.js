bef13383d9c6d0c90fdf4472caf120ce929dce43
luantreia <luantreia@gmail.com>
2025-05-17 00:04:31 -0300

    cambio en firebaseadmin

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -2,10 +2,9 @@
 const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-if (!admin.apps.length) {
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
 
 export default admin;
