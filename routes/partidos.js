765355ca9868d18ddcf82d7a57bf3bfa10198764
luantreia <luantreia@gmail.com>
2025-06-20 12:48:12 -0300

    Auto deploy

--- routes/partidos.js
+++ routes/partidos.js
@@ -17,9 +17,9 @@ import { esAdminDeEntidad } from '../middlewares/esAdminDeEntidad.js';
 const router = express.Router();
 
 router.get('/', obtenerPartidos);
-router.get('/:id', obtenerPartidoPorId);
-router.post('/', validarObjectId, verificarToken, crearPartido);
-router.put('/:id', verificarToken, esAdminDeEntidad(Partido, 'partido'), actualizarPartido);
+router.get('/:id', validarObjectId, obtenerPartidoPorId);
+router.post('/', verificarToken, crearPartido);
+router.put('/:id', validarObjectId, verificarToken, esAdminDeEntidad(Partido, 'partido'), actualizarPartido);
 router.post('/:id/sets', verificarToken, esAdminDeEntidad(Partido, 'partido'), agregarSet);
 router.put('/:id/sets/:numeroSet/stats', verificarToken, esAdminDeEntidad(Partido, 'partido'), actualizarStatsSet);
 router.put('/:id/sets/:numeroSet', verificarToken, esAdminDeEntidad(Partido, 'partido'), actualizarSet);
