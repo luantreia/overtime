e83ad2c35e92df39291b5211c4647c133c1cfc98
luantreia <luantreia@gmail.com>
2025-07-15 15:18:00 -0300

    Auto deploy

--- routes/Equipos/participacionTemporada.js
+++ routes/Equipos/participacionTemporada.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import ParticipacionTemporada from '../../models/ParticipacionTemporada.js';
+import ParticipacionTemporada from '../../models/Equipo/ParticipacionTemporada.js';
 import verificarToken from '../../middlewares/authMiddleware.js';
 import { validarObjectId } from '../../middlewares/validacionObjectId.js';
 
