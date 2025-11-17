b16189d7ead09b4272b0f0484a081d6a61f9d769
luan <luantreia@gmail.com>
2025-11-17 19:07:19 -0300

    Auto deploy

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.use(helmet());
 // Rate Limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 1000 requests per windowMs
   standardHeaders: true,
   legacyHeaders: false,
   message: 'Too many requests from this IP, please try again after 15 minutes',
