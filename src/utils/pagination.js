a1f2f0a735a60fc2e88510310b456927e3e6f953
luan <luantreia@gmail.com>
2026-01-26 04:18:10 -0300

    a

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,6 +1,6 @@
 export const getPaginationParams = (req) => {
   const page = Math.max(1, parseInt(req.query.page) || 1);
-  const limit = Math.min(100, parseInt(req.query.limit) || 20); // Máx 100
+  const limit = Math.min(1000, parseInt(req.query.limit) || 20); // Máx 1000
   const skip = (page - 1) * limit;
   return { page, limit, skip };
 };
